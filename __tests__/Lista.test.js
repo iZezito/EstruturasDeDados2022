416de52cf70e0041891064ce72e0653430b0cdc2
iZezito <emersonsilva81240@gmail.com>
2022-04-17 20:43:36 -0300

    Lista

---
+++ __tests__/Lista.test.js
@@ -0,0 +1,24 @@
+import Lista from "../src/Lista";
+import {ListaArrayNodes} from "../src/ClassesListas";
+
+let l
+
+beforeEach(
+    ()=>{
+        l = new Lista()
+    }
+)
+
+
+test("invertedor", ()=>{
+    l.append(1)
+    l.append(2)
+    l.append(3)
+    l.append(4)
+    l.append(5)
+    l.inverterOrdem()
+
+    console.log(l.toString())
+
+    expect(l.inverterOrdem()).toBe(l.removeEnd() === 5)
+})
